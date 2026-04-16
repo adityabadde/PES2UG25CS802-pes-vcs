2d1808215f1431c27be606426fcc09ac88249533
adityabadde <107803310+adityabadde@users.noreply.github.com>
2026-04-16 15:48:56 +0530

    Update commit.c

--- commit.c
+++ commit.c
@@ -1,4 +1,5 @@
-// commit.c — Commit creation and history traversal
+
+// // commit.c — Commit creation and history traversal
 //
 // Commit object format (stored as text, one field per line):
 //
@@ -8,10 +9,7 @@
 //   committer <name> <unix-timestamp>
 //
 //   <commit message>
-//
-// Note: there is a blank line between the headers and the message.
-//
-// PROVIDED functions: commit_parse, commit_serialize, commit_walk, head_read, head_update
+//tions: commit_parse, commit_serialize, commit_walk, head_read, head_update
 // TODO functions:     commit_create
 
 #include "commit.h"
